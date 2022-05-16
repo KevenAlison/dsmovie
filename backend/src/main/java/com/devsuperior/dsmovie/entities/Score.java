f8769972410a14d107dcb4210f4b6c62fbc5864a
KevenAlison <kevenalison13@gmail.com>
2022-05-16 01:20:20 -0300

    add score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -2,9 +2,6 @@ package com.devsuperior.dsmovie.entities;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 
